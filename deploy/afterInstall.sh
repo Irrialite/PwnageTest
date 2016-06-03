6272265aa445f552a20b8e4fdad053cfd1bea07b
Crt <crt@ibzoot.com>
2016-06-03 10:56:24 +0200

    don't clear log?

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /app/log
 	
-sudo rsync --delete-before --verbose --archive --exclude ".*" /app/clientTemp/ /app/client/ > /app/log/deploy.log
+sudo rsync --delete-before --verbose --archive --exclude ".*" --exclude "log" /app/clientTemp/ /app/client/ > /app/log/deploy.log
